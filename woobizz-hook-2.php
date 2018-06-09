0d97663c45d047af45dad6b85f8f4f75a32fb401
roiwebco <30891172+roiwebco@users.noreply.github.com>
2018-06-09 15:01:44 +0200

    Change Brand and some small fixes

--- woobizz-hook-2.php
+++ woobizz-hook-2.php
@@ -3,9 +3,9 @@
 Plugin Name: Woobizz Hook 2 
 Plugin URI: http://woobizz.com
 Description: Hide order notes on checkout page
-Author: Woobizz
+Author: WOOBIZZ.COM
 Author URI: http://woobizz.com
-Version: 1.0.0
+Version: 1.0.1
 Text Domain: woobizzhook2
 Domain Path: /lang/
 */
@@ -16,22 +16,8 @@ add_action( 'plugins_loaded', 'woobizzhook2_load_textdomain' );
 function woobizzhook2_load_textdomain() {
   load_plugin_textdomain( 'woobizzhook2', false, basename( dirname( __FILE__ ) ) . '/lang' ); 
 }
-//Add Hook 2
-//add_filter( 'woocommerce_enable_order_notes_field', '__return_false', 1);
-//Check if WooCommerce is active
-if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
-	//echo "woocommerce is active";
+//Hook 2
+function woobizzhook2_disable_order_notes() {
     add_filter( 'woocommerce_enable_order_notes_field', '__return_false', 1);
-}else{
-	//Show message on admin
-	//echo "woocommerce is not active";
-	add_action( 'admin_notices', 'woobizzhook2_admin_notice' );
 }
-//Hook2 Notice
-function woobizzhook2_admin_notice() {
-    ?>
-    <div class="notice notice-error is-dismissible">
-        <p><?php _e( 'Woobizz hook 2 needs woocommerce to work properly, please install and activate woocommerce or disable this plugin.', 'woobizzhook2' ); ?></p>
-    </div>
-    <?php
-}
+add_action( 'init', 'woobizzhook2_disable_order_notes' );
